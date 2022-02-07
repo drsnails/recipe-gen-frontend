69ea35ec48e5b0384228255ddc3ff8c58b46bc99
Stav Yaar Bar <anistu@gmail.com>
2022-02-08 01:35:42 +0200

    for merging

--- src/cmps/RecipePreview.jsx
+++ src/cmps/RecipePreview.jsx
@@ -8,14 +8,22 @@ export function RecipePreview({ recipe, removeRecipe, copyRecipe }) {
     const navigate = useNavigate()
 
 
-
+    const placeholderImgUrl = require('../assets/imgs/pepper-logo.png')
+    const noImgClass = recipe.imgUrl ? '' : 'no-img'
     return (
         <section key={recipe._id} onClick={() => navigate(`recipe/${recipe._id}`)} className='recipe-preview'>
-            <section className="title">
+            <section style={{ backgroundImage: `url(${recipe.imgUrl || placeholderImgUrl})` }} className={`img-container ${noImgClass}`}>
+                <section className="title-container">
+                    <span className='title'>
+                        {recipe.name}
+                    </span>
+                </section>
+            </section>
+            {/* <section className="title">
                 <span>
                     {recipe.name}
                 </span>
-            </section>
+            </section> */}
             <section onClick={(ev) => ev.stopPropagation()} className='actions'>
                 <button onClick={() => copyRecipe(recipe)} className='copy-recipe-icon'><FontAwesomeIcon icon={faCopy} /></button>
                 <button onClick={() => removeRecipe(recipe._id, recipe.name)} className="remove-recipe-icon" ><FontAwesomeIcon icon={faTrash} /></button>
